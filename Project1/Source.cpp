8ccdd26403286c168656826a5822de44bf2f7c1e
Sergio Mrtin <sergio.ms.91@gmail.com>
2017-08-23 12:49:10 +0100

    PLaying with colors

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -12,15 +12,20 @@ const unsigned int SCR_HEIGHT = 600;
 
 const char *vertexShaderSource = "#version 330 core\n"
 "layout (location = 0) in vec3 aPos;\n"
+"layout (location = 1) in vec3 aColor;\n"
+"out vec3 ourColor;\n"
 "void main()\n"
 "{\n"
-"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"   gl_Position = vec4(aPos, 1.0);\n"
+"   ourColor = aColor;\n"
 "}\0";
+
 const char *fragmentShaderSource = "#version 330 core\n"
 "out vec4 FragColor;\n"
+"in vec3 ourColor;\n"
 "void main()\n"
 "{\n"
-"   FragColor = vec4(0.2f, 0.6f, 0.5f, 0.1f);\n"
+"   FragColor = vec4(ourColor, 1.0f);\n"
 "}\n\0";
 
 int main()
@@ -99,20 +104,29 @@ int main()
 
 	// set up vertex data (and buffer(s)) and configure vertex attributes
 	// ------------------------------------------------------------------
+	/*float vertices[] = {
+		// positions         // colors
+		0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,   // bottom right
+		-0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f,   // bottom left
+		0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f    // top 
+	};*/
+	
 	float vertices[] = {
-		0.5f,  0.5f, 0.0f,  //Vertex 0
-		0.5f, -0.5f, 0.0f,  //Vertex 1
-		-0.5f, -0.5f, 0.0f, //Vertex 2
-		-0.5f,  0.5f, 0.0f, //Vertex 3  
-		-1.0f, -0.5f, 0.0f, //Vertex 4
-		1.00f, 0.5f, 0.0f   //Vertex 5
+		//   0   1   2   
+		//3  4   5
+		-0.5f,  0.5f, 0.0f,      1.0f, 0.0f, 0.0f,  //Vertex 0
+		0.5f,  0.5f, 0.0f,       0.0f, 1.0f, 0.0f,  //Vertex 1
+		1.00f, 0.5f, 0.0f,       0.0f, 1.0f, 0.0f,   //Vertex 2
+		- 1.0f, -0.5f, 0.0f,     1.0f, 0.0f, 0.0f,  //Vertex 3
+		-0.5f, -0.5f, 0.0f,      1.0f, 0.0f, 0.0f,  //Vertex 4
+		0.5f, -0.5f, 0.0f,       0.0f, 1.0f, 0.0f  //Vertex 5
+
 	};
 	unsigned int indices[] = {  // note that we start from 0!
-		0, 1, 3, //1st Triangle
-		1, 2, 3, //2nd Triangle
-		4, 3, 2, //3rd Triangle
-		5, 0, 1  //4th Triangle
-
+		0, 3, 4, //1st Triangle
+		0, 4, 5, //2nd Triangle
+		0, 5, 1, //3rd Triangle
+		1, 5, 2  //4th Triangle
 	};
 
 	int num_triangle = sizeof(indices) / 3;
@@ -130,8 +144,13 @@ int main()
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	/*glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);*/
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
 
 	// note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
