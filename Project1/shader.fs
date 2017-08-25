2c545935b80f1ca4ecea37fcf33f7b46c83099ac
Sergio Mrtin <sergio.ms.91@gmail.com>
2017-08-25 11:12:27 +0100

    Mixing Textures with colors

--- Project1/shader.fs
+++ Project1/shader.fs
@@ -1,12 +1,5 @@
 #version 330 core
-/*out vec4 FragColor;
 
-in vec3 ourColor;
-
-void main()
-{
-    FragColor = vec4(ourColor, 1.0f);
-}*/
 out vec4 FragColor;
 
 in vec3 ourColor;
@@ -17,5 +10,5 @@ uniform sampler2D texture1;
 
 void main()
 {
-	FragColor = texture(texture1, TexCoord);
+	FragColor = texture(texture1, TexCoord) * vec4(ourColor, 1.0);
 }
