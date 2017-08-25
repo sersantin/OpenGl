9d08b055545dafa9c7fdc57d987ac8fe9854cc4d
Sergio Mrtin <sergio.ms.91@gmail.com>
2017-08-25 10:56:28 +0100

    Appling Textures on a Triangle

---
+++ Project1/shader.fs
@@ -0,0 +1,21 @@
+#version 330 core
+/*out vec4 FragColor;
+
+in vec3 ourColor;
+
+void main()
+{
+    FragColor = vec4(ourColor, 1.0f);
+}*/
+out vec4 FragColor;
+
+in vec3 ourColor;
+in vec2 TexCoord;
+
+// texture sampler
+uniform sampler2D texture1;
+
+void main()
+{
+	FragColor = texture(texture1, TexCoord);
+}
